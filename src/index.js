ad6b32e33857955beb558f849cc8b5375af8f2f2
Andrew LeRoux <andrewleroux2639@gmail.com>
2021-12-08 14:53:13 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', function(event) {
+    //prevents default action of submit event
+    event.preventDefault()
+
+    createTasks(event.target.newTaskDescription.value)
+    
+  })
 });
+
+
+function createTasks(task){
+  
+  // p is <p> element, btn is <btn> element
+  let p = document.createElement('p')
+  let btn = document.createElement('btn')
+
+  // content for p and btn
+  p.textContent = `${task} `
+  btn.textContent = '|x|'
+ 
+  p.appendChild(btn)
+  btn.addEventListener('click', event => event.target.parentNode.remove())
+  
+  document.querySelector('#tasks').appendChild(p)
+}
